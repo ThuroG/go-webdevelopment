e71caf91755aa592d0454711b20d38657600ee86
ThuroG <thuro.g.7@gmail.com>
2025-02-14 11:09:14 +0700

    Section 4: Create experimental template

---
+++ cmd/exp/exp.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"html/template"
+)
+
+type User struct {
+	Name string
+	Age int
+}
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "John Doe",
+		Age: 123,
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
