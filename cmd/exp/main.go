56f664f86205b7cc8ae6de6694dfe84e543f40b4
ThuroG <thuro.g.7@gmail.com>
2025-02-15 22:54:53 +0700

    Section 4 - Exercise in Exp

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -9,17 +9,32 @@ type User struct {
 	Name string
 	Bio string
 	Age int
+	Address Address
 }
+
+type Address struct {
+	Street string
+	ZipCode string
+	City string
+}
+
 func main() {
 	t, err := template.ParseFiles("hello.gohtml")
 	if err != nil {
 		panic(err)
 	}
 
+	address := Address{
+		Street: "123 Main St",
+		ZipCode: "12345",
+		City: "Springfield",
+	}
+
 	user := User{
-		Name: "John Doe",
+		Name: "Arthur",
 		Bio: `alert("Haha, you have been h4x0r3d!");`,
 		Age: 123,
+		Address: address,
 	}
 
 	err = t.Execute(os.Stdout, user)
