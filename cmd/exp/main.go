1248a8c9774c6948f2881b0538963484b97c08ad
ThuroG <thuro.g.7@gmail.com>
2025-03-12 15:18:26 +0700

    added postgresql and experimental code to test Go SQL connection

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"embed"
 	"errors"
 	"fmt"
 	"html/template"
 	"os"
-	"embed"
 
-
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 //go:embed *
@@ -26,6 +27,19 @@ type Address struct {
 	Street string
 	ZipCode string
 	City string
+}
+
+type PostgresConfig struct {
+	Host string
+	Port string
+	User string
+	Password string
+	Database string
+	SSLmode string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLmode)
 }
 
 func Connect() error {
@@ -85,6 +99,26 @@ func main() {
 	} else {
 		fmt.Println(err)
 	}
+
+	cfg := PostgresConfig{
+		Host: "localhost",
+		Port: "5432",
+		User: "thuro",
+		Password: "junglebook",
+		Database: "webapp",
+		SSLmode: "disable",
+	}
+	// Use pgx in order to connect to Postgresql
+	db, err := sql.Open("pgx", cfg.String())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close() //Close connection if err occurred
+	err = db.Ping() // throws an error if not pingable
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected")
   }
   
 
