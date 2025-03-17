fb544959667a5a1899c692d4e2ba657d9f349143
ThuroG <thuro.g.7@gmail.com>
2025-03-17 22:08:16 +0700

    finish section 11 - SignIn Page

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"go-webdevelopment/models"
 	"net/http"
 )
@@ -8,6 +9,7 @@ import (
 type Users struct {
 	Templates struct {
 		New Template
+		SignIn Template
 	}
 	UserService *models.UserService
 }
@@ -23,5 +25,35 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("email") 
 	password := r.FormValue("password")
-	w.Write([]byte(email + " " + password))
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Created user: %v", user)
+}
+
+func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.SignIn.Execute(w, data)
+}
+
+func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+		Password string
+	}
+	data.Email = r.FormValue("email")
+	data.Password = r.FormValue("password")
+	user, err := u.UserService.Authenticate(data.Email, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Authenticated user: %v", user)
 }
--- models/user.go
+++ models/user.go
@@ -49,6 +49,27 @@ func (us *UserService) Create(email, password string) (*User, error) {
 	return &user, nil //Return the pointer reference
 }
 
+func (us *UserService) Authenticate(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+	user := User{
+		Email: email,
+	}
+	row := us.DB.QueryRow(`
+	SELECT id, password_hash
+	FROM users
+	WHERE email = $1`, email)
+	err := row.Scan(&user.ID, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("could not find user with email %q: %w", email, err)
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return nil, fmt.Errorf("invalid password: %w", err)
+	}
+	fmt.Println("Password is correct")
+	return &user, nil
+}
+
 func (us *UserService) Update(user *User) error {
 	//TODO implement
 	return nil
